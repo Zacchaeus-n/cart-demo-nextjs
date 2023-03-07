2d9d3124d77218ff9c1fc6fb2a2ffe159a70d182
Zacchaeus Napuo <zacchaeus.napuo@amalitech.org>
2023-03-07 15:56:23 +0000

    converting app to experimental next13

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+    <title>Cart Demo Project</title>
+    <meta content='width=device-width,intial-scale-1' name='viewport' />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    </>
+  )
+}
+
+export default Head
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className=''>
+        <h1 className='text-xl'>Welcome to our blog</h1>
+    </div>
+  )
+}
+
+export default HomePage
